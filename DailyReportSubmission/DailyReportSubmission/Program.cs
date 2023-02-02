0669242dd642d87f7c45c8b366b42d61b33f3158
veepham <111474183+veepham@users.noreply.github.com>
2023-02-02 15:13:08 -0800

    Daily Report Submission
    Created a console app for daily report submission

---
+++ DailyReportSubmission/DailyReportSubmission/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReportSubmission
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("The Tech Academy");
+            Console.ReadLine();
+
+            Console.WriteLine("Student Daily Report");
+            Console.ReadLine();
+
+            Console.WriteLine("What is your name?");
+            Console.ReadLine();
+
+            Console.WriteLine("What course are you on?");
+            Console.ReadLine();
+
+            Console.WriteLine("What page number?");
+            Console.ReadLine();
+
+            Console.WriteLine("Do you need help with anything? Please answer true or false.");
+            Console.ReadLine();
+
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study?");
+            Console.ReadLine();
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+
+        }
+    }
+}
