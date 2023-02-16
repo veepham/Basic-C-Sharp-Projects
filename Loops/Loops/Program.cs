b1a19ebd0e3ad526cf752d0934e8bebe39812ac2
veepham <111474183+veepham@users.noreply.github.com>
2023-02-15 19:46:54 -0800

    Loops
    Console app utilizing while, do/while statements

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 44;
+
+            do //do, while loop allows switch statement to run in case the correct answer is inputted first
+            {
+                switch (number)
+                {
+                    case 33:
+                        Console.WriteLine("You guessed 33. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 22:
+                        Console.WriteLine("You guessed 22. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 88:
+                        Console.WriteLine("You guessed 88. Try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 44:
+                        Console.WriteLine("You guessed 44. That is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong, try again!");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed); //while loop when "isGuessed" is FALSE
+            {
+                
+            }
+
+            
+            Console.Read();
+        }
+    }
+}
