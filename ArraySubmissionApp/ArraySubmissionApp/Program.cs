07f3b2aea979759b296b69c4455da94961fc969a
veepham <111474183+veepham@users.noreply.github.com>
2023-02-18 17:23:07 -0800

    Array Submission Assignment
    Console app displaying use of arrays and lists

---
+++ ArraySubmissionApp/ArraySubmissionApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySubmissionApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] stringArray = { "cat", "dog", "cow", "chicken", "fox" };
+
+            Console.WriteLine("Pick a number 0-4 to display a word.");
+            int chosenNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The position you selected contains the animal: " + stringArray[chosenNum]);
+            Console.ReadLine();
+
+            int[] numArray = { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine("Pick a number to display a number from the array.");
+            int arrayNum = Convert.ToInt32(Console.ReadLine());
+            if (arrayNum < 6) 
+            {
+                Console.WriteLine("The position you selected contains the number: " + numArray[arrayNum]);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("This position does not exist in the array. Better luck next time.");
+                Console.ReadLine();
+            }
+            
+            List<string> list = new List<string>();
+            list.Add("This is a sentence.");
+            list.Add("How are you today?");
+            list.Add("The sky is blue.");
+
+            Console.WriteLine("Pick a number from 0-2 for a random sentence.");
+            int indexNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(list[indexNum]);
+            Console.ReadLine();
+        }
+    }
+}
