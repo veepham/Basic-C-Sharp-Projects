2a9c4ee13f60a6d6dc5671d4c0980828cb004ad9
veepham <111474183+veepham@users.noreply.github.com>
2023-02-19 20:14:48 -0800

    Exception Handling - Try and Catch Blocks
    Using try and catch blocks to throw exception messages and using "finally" to indicate emerging from try/catch block to continue on with program

---
+++ StringsAndIntegers/StringsAndIntegers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAndIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> myNumbers = new List<int>() { 98, 99, 12, 74, 23, 59 };
+
+                Console.WriteLine("Choose a number to divide each number in our list by.");
+                int yourNumber = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int number in myNumbers)
+                {
+                    int result = number / yourNumber;
+                    Console.WriteLine(number + " divide by " + yourNumber + " equals to " + result);
+                }
+            }
+
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message + "\nPlease type a whole number.");
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero.");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Thanks for playing the game!");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
