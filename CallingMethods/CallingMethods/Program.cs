c4b12dab30824cc1e7c80ff9070174c466ad1a10
veepham <111474183+veepham@users.noreply.github.com>
2023-02-19 22:10:58 -0800

    Calling Methods
    Create a class with 3 mathematical methods that can be called in main program to perform function on user inputted number

---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give me a number.");
+            int yourNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Calulating your results...");
+            Console.ReadLine();
+
+            //calling each method and passing in user input
+            newClass math = new newClass();
+            int result1 = math.Addition(yourNumber);
+            int result2 = math.Subtraction(yourNumber);
+            int result3 = math.Multiplication(yourNumber);
+
+            Console.WriteLine("100 plus your chosen number is equal to " + result1);
+            Console.ReadLine();
+
+            Console.WriteLine("100 minus your chosen number is equal to " + result2);
+            Console.ReadLine();
+
+            Console.WriteLine("100 times your chosen number is equal to " + result3);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/newClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class newClass
+    {
+        public int Addition(int yourNumber) 
+        {
+            return 100 + yourNumber;
+        }
+
+        public int Subtraction(int yourNumber)
+        {
+            return 100 - yourNumber;
+        }
+
+        public int Multiplication(int yourNumber)
+        {
+            return 100 * yourNumber;
+        }
+
+    }
+}
