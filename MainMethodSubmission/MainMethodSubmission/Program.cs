ee6edea11c602f4e2e10e332abade6f7a3e5aa44
veepham <111474183+veepham@users.noreply.github.com>
2023-02-20 21:06:08 -0800

    Main method submission
    Method overload

---
+++ MainMethodSubmission/MainMethodSubmission/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter any whole number.");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            mainClass math = new mainClass(); //instantiate the class
+            Console.WriteLine(math.Addition(x)); // calling method and passing in the integer
+            Console.ReadLine();
+
+
+            Console.WriteLine("Please enter a decimal value.");
+            decimal y = Convert.ToDecimal(Console.ReadLine());
+
+            mainClass decimalMath = new mainClass(); //instantiate the class
+            Console.WriteLine(math.Addition(y)); // calling method and passing in the integer
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter a whole number.");
+            string z = Console.ReadLine();
+
+            mainClass stringMath = new mainClass();
+            Console.WriteLine(stringMath.Addition(z));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MainMethodSubmission/MainMethodSubmission/mainClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    public class mainClass
+    {
+        public int Addition (int x) 
+        {
+            return x + 100; 
+        }
+
+        public int Addition (decimal x)
+        {
+            int y = Convert.ToInt32(x);
+            return y + 500;
+        }
+
+        public int Addition (string x)
+        {
+            int z = Convert.ToInt32(x);
+            return z + 200;
+        }
+    }
+}
