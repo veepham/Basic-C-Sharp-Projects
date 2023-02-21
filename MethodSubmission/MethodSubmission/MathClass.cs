286c3ad6116fe5a3a32bd9029311680ad0faa0d4
veepham <111474183+veepham@users.noreply.github.com>
2023-02-20 21:29:14 -0800

    Method Submission
    Creating a method that takes in two parameters, one of which is optional. User can input up to two numbers to perform a math function

---
+++ MethodSubmission/MethodSubmission/MathClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    public class MathClass
+    {
+        public int Multiply (int x, int y = 1)
+        {
+            return x * y * 100;
+        }
+    }
+}
---
+++ MethodSubmission/MethodSubmission/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathClass math = new MathClass();
+            
+
+            Console.WriteLine("Please enter your first number.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your second number (optional - click enter to surpass)");
+            string y = Console.ReadLine();
+            Console.WriteLine("Calculating...");
+
+            if (y.Equals(string.Empty))
+            {
+                Console.WriteLine("Result: " + math.Multiply(x));
+            }
+            else
+            {
+                int z = Convert.ToInt32(y);
+                Console.WriteLine(math.Multiply(x, z));
+            }            
+            Console.ReadLine();
+        }
+    }
+}
