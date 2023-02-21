8ec0b5da2e17e29fe2a1e2d399c50de71d571bc0
veepham <111474183+veepham@users.noreply.github.com>
2023-02-20 22:55:39 -0800

    Class Submission

---
+++ ClassSubmission/ClassSubmission/Half.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    public class Half
+    {
+
+        public void halfMath(int input, out int output)
+        {
+            output = input / 2;
+            Console.WriteLine(input + " divided by 2 equals " + output);
+        }
+
+        public void halfMath(decimal input, out decimal output)
+        {
+            output = input / 2;
+            Console.WriteLine(input + " divided by 2 equals " + output);
+        }
+
+    }
+}
---
+++ ClassSubmission/ClassSubmission/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Half math = new Half();
+            
+            Console.WriteLine("Enter a number to divide by 2:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int output;
+            math.halfMath(x, out output);
+
+            Console.ReadLine();
+
+            Half math2 = new Half();
+
+            Console.WriteLine("Enter a decimal number to divide by 2:");
+            decimal y = Convert.ToDecimal(Console.ReadLine());
+            decimal output2;
+            math2.halfMath(y, out output2);
+
+            Console.ReadLine();
+        }
+    }
+}
