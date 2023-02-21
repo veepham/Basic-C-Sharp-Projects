991cfea76feb8cad89faba9161b380e05d15e1c3
veepham <111474183+veepham@users.noreply.github.com>
2023-02-20 22:01:48 -0800

    Creating a void method
    Passing in two integers as parameters

---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter another number:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            myClass mathMethod = new myClass(); //instantiate
+            int result; // defining variable used in "out"
+            mathMethod.Math(x, y, out result); // calling method and specifying the parameters
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClass/MethodClass/myClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    public class myClass
+    {
+        public void Math(int x, int y, out int result)
+        {
+            result = x * 100;
+            Console.WriteLine(x + " times 100 is equal to " + result);
+            Console.WriteLine("The second number you entered is: " + y);
+            
+        }
+    }
+}
