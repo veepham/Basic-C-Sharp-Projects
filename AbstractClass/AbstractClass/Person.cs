57d8df517f847de738d565bfed8b0451c3a2c00f
veepham <111474183+veepham@users.noreply.github.com>
2023-02-23 16:47:15 -0800

    Abstract
    Using abstract class and virtual method and overriding it in inherited class

---
+++ AbstractClass/AbstractClass/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public virtual void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee() { firstName = "Sample", lastName = "Student" };
+            person.SayName();
+            Console.ReadLine();
+        }
+    }
+}
