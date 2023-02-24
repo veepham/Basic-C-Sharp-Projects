9e454fd932f5d0499605e85959ee36d679240179
veepham <111474183+veepham@users.noreply.github.com>
2023-02-23 20:47:08 -0800

    Using generic type parameter

---
+++ Parameters/Parameters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> myList = new Employee<string>();
+            myList.Things = new List<string>() { "hey", "how are you", "whats new" };
+
+            Employee<int> Id = new Employee<int>();
+            Id.Things = new List<int>() { 1, 2, 3, 4 };
+
+            foreach (var thing in myList.Things)
+            {
+                Console.WriteLine(thing);
+                Console.ReadLine();
+            }
+            foreach (var thing in Id.Things)
+            {
+                Console.WriteLine(thing);
+                Console.ReadLine();
+            }
+        }
+    }
+}
