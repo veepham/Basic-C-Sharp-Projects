c83f8256ea29784e4c20bf3afa14fe06f2350920
veepham <111474183+veepham@users.noreply.github.com>
2023-02-23 20:20:14 -0800

    Overload == operator

---
+++ OverloadOperators/OverloadOperators/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperators
+{
+    public class Employee : Person
+    {        
+        public int Id { get; set; }
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {            
+            if (employee1.Id == employee2.Id)
+            {
+                return employee1.Id == employee2.Id;
+            }
+            else
+            {
+                return false;
+            }           
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return employee1 != employee2;
+                      
+        }
+
+        public override bool Equals(Object obj)
+        {
+            Employee employee = obj as Employee;
+            if (employee == null)
+            {
+                return false;
+            }
+            return this.Id.Equals(employee.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ OverloadOperators/OverloadOperators/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperators
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee em1 = new Employee() { FirstName = "Bob", LastName = "Smith", Id = 1 };
+            Employee em2 = new Employee() { FirstName = "Bob", LastName = "Jones", Id = 2 };
+            em1 == em2;
+            
+        }
+    }
+}
