ab58dc46152101913cd71416d16b53fcefc51f9d
veepham <111474183+veepham@users.noreply.github.com>
2023-02-23 17:13:10 -0800

    Using Interfaces
    Creating an interface for a class to inherit. Using polymorphism, create an object of interface

---
+++ Polymorphism/Polymorphism/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("I QUIT");
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable game = new Employee();
+            game.Quit();
+            Console.ReadLine();
+
+        }
+    }
+}
