8d4524f53edbd4df7f88208aa461fdc58e6e7478
veepham <111474183+veepham@users.noreply.github.com>
2023-03-02 20:08:23 -0800

    Date Time
    Using date time to find current time and using User Input to show time in X hours later

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dtSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("It is currently " + DateTime.Now);
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter a number from 1-12.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            DateTime later = DateTime.Now.AddHours(x);
+
+            Console.WriteLine("In " + x + " hour(s), it will be " + later);
+            Console.ReadLine();
+
+        }
+    }
+}
