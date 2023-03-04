72ff8c0cc0f700e576527c0e056b692710612f46
veepham <111474183+veepham@users.noreply.github.com>
2023-03-03 21:21:37 -0800

    Constructor Submission
    Using const variables, var keyword, and chaining two constructors together

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Company = "The School"; // create const variable
+            var newStudent = new Student("Jack"); // Create a variable using the keyword “var.”
+            Console.WriteLine("Welcome to {0}! ", Company);
+            Console.WriteLine(newStudent.name + " is present.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Constructor/Constructor/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    public class Student
+    {
+        public string name;
+        public string location;
+
+        public Student(string name) : this(name, "Online") { } //Chain two constructors together.
+        public Student(string name, string location) 
+        {
+            this.name = name;
+            this.location = location;
+        }
+    }
+}
