f44899bebe0dccda890911c743cb992e299f015c
veepham <111474183+veepham@users.noreply.github.com>
2023-03-16 16:29:09 -0700

    Console App Database from Code First
    Using entity from code first to create a database

---
+++ XYZSchool/XYZSchool/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace XYZSchool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var db = new SchoolContext())
+            {
+                var student = new Student() 
+                { 
+                    StudentName = "Katie" 
+                };
+
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                var query = from c in db.Students
+                            orderby c.StudentName
+                            select c;
+
+                Console.WriteLine("All students in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.StudentName);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+
+            
+        }
+    }
+
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        public Grade Grade { get; set; }
+    }
+
+    public class Grade
+    {
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public string Section { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
